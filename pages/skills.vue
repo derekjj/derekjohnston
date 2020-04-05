951a9c87e0bc6122ec6b60f5d56ad32755003231
Derek Johnston <Derek@dj-design.ca>
2020-04-04 20:22:55 -0400

    cheanged about to be skills and removed junior txt

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,20 +1,7 @@
 <template lang="pug">
-  .container-fluid
-    .card
-      .card-body
-        .p I have been immersed in the technology world since the age of DOS; progressing through DOSSHELL to Windows 3.1 all the way to Windows 10. 
-        br
-        .p My parents both influenced me into the technology field; my mom was a computer technician and my father was a telephony technician.
-        br
-        .p Well attending Durham College, as a Computer Programming Analyst, I have reinforced my team working and leadership skills, at the same time improving my time management.
-        br
-        .p In addition to all the knowledge I gained as a student at Durham college, where I was on the honor roll several times.  
-        
-        .p I reinforced and expanded on it, while working at E-Data Now! and Oiika.
-        
-        .p I have also have experience from my high school co-op position as Site Admin Tech Assistant and I am keeping active on several side projects.
+  .container
     .row
-      .col-sm-6.my-3(v-for="skill in skills")
+      .col-sm-6.mb-3(v-for="skill in skills")
         .card.h-100
           .card-header.text-center
             h4.card-title
