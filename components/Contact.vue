bf18b04b2611ac255c40788b7b42d052434b026e
Derek Johnston <Derek@dj-design.ca>
2020-05-15 01:48:58 -0400

    made contack page a component

---
+++ components/Contact.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  .card
+    .card-header
+      H1 Contact
+    .card-body
+      .row.m-1
+        .col-lg-12
+          img.rounded-circle(src="~assets/me2.jpg" alt="Derek Johnston")
+        .col-lg-12.pt-5
+          h2 Derek Johnston
+          h6
+            |  Software Developer
+          h6
+            a(href="mailto:Derek@DerekJohnston.ca") 
+              span.fa.fa-envelope(data-toggle="tooltip" title="Derek@DerekJohnston.ca")
+              |  Derek@DerekJohnston.ca
+          <form action="https://formspree.io/derek@derekjohnston.ca" method="post">
+            <div class="form-group">
+                <input type="text" id="name" name="name" class="form-control" placeholder="Name">
+            </div>
+            <div class="form-group">
+                <input type="text" id="email" name="email" class="form-control" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <input type="text" id="subject" name="_subject" class="form-control" placeholder="Subject">
+            </div>
+            <div class="form-group">
+                <textarea id="message" name="message" cols="30" rows="7" class="form-control" placeholder="Message..."></textarea>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary btn-send-message" value="Send Message">
+            </div>
+          </form>
+</template>
+<script>
+//TODO: add from validation 
+</script>
+<style scoped>
+a {
+  color: black;
+}
+.successful-server-response-wrapper {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+.successful-server-response {
+  text-align: center;
+  max-width: 500px;
+  font-size: 36px;
+  background-color: #5cb85c;
+  color: #f1f1f1;
+  padding: 20px;
+  border-radius: 3px;
+}
+</style>
