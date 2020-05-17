2d569d654d093703060c3246510d2d805d94c962
Derek Johnston <Derek@dj-design.ca>
2020-05-17 16:55:52 -0400

    sepetated the two different navs for readability

--- components/Header.vue
+++ components/Header.vue
@@ -2,6 +2,7 @@
   b-navbar.bg-success(toggleable="sm" type="dark" fixed="top")
     b-button.d-none.d-sm-block.mr-2(v-b-toggle.sidebar-variant variant="success")
       .navbar-toggler-icon
+      
     b-navbar-toggle.d-sm-none(target="nav-collapse")
     b-collapse(id="nav-collapse" is-nav)
       b-navbar-nav.ml-auto.d-sm-none
