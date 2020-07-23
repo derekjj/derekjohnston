b92fac8a0746a0ba9857378101057ac8a3873b6b
Derek Johnston <Derek@dj-design.ca>
2020-07-23 09:52:21 -0400

    made the intro into its on component

---
+++ components/Intro.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  transition(name="fade")  
+    .card(v-if="isShowing")
+      b-img.card-image-top(:src="require('!!url-loader?limit=0!~/assets/tech-bg.jpg')" fluid)
+      .card-body.m-3
+        p
+          i “Any fool can write code that a computer can understand. Good programmers write code that humans can understand.”
+          br
+          | ― Martin Fowler
+        br
+        | Currently developing projects with Docker, NuxtJS, VueJS, ReactJS, NodeJS, Flutter(Dart), Ruby on Rails & PHP.
+        br
+        br
+        p
+          b Seeking employment opportunities remotely or Toronto to Durham and surrounding areas.
+        br
+        br
+        a(href="/DerekJohnstonNoCell.pdf") Resume
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isShowing: false
+    };
+  },
+  mounted() {
+    this.isShowing = true;
+  }
+};
+</script>
