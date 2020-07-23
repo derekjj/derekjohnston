bcd72a6285e04b62202a4c36e624375a54aae574
Derek Johnston <Derek@dj-design.ca>
2020-07-23 09:56:45 -0400

    moved intro to a component

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,23 @@
 <template lang="pug">
   .container
-    .card
-      b-img.card-image-top(:src="require('!!url-loader?limit=0!~/assets/tech-bg.jpg')" fluid)
-      .card-body.m-3
-        p
-          i “Any fool can write code that a computer can understand. Good programmers write code that humans can understand.”
-          br
-          | ― Martin Fowler
-        br
-        | Currently developing projects with Docker, NuxtJS, VueJS, ReactJS, NodeJS, Flutter(Dart), Ruby on Rails & PHP.
-        br
-        br
-        p
-          b Seeking employment opportunities remotely or Toronto to Durham and surrounding areas.
-        br
-        br
-        a(href="/DerekJohnstonNoCell.pdf") Resume
+    Intro
     Projects
     Knowledge
     Contact
 </template>
 <script>
 //TODO: Direct to resume builder, and add print to pdf option
-import Projects from '~/components/Projects'
-import Knowledge from '~/components/Knowledge'
-import Contact from '~/components/Contact'
+import Intro from "~/components/Intro";
+import Projects from "~/components/Projects";
+import Knowledge from "~/components/Knowledge";
+import Contact from "~/components/Contact";
 export default {
   components: {
+    Intro,
     Projects,
     Knowledge,
     Contact
-  },
+  }
 };
 </script>
 <style scoped>
